a4ebb040d6da236e9f7d35ac92e47df957ca7b08
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2013-06-12 12:59:56 +0100

    Asynchronous

--- js/tests/dummyTest.js
+++ js/tests/dummyTest.js
@@ -5,33 +5,31 @@ define(
     function(dummyLib) {
         
         var run = function() {
-            module('List', {
-                setup: function(){
-                    console.log('setup');
-                },
-                teardown: function(){
-                    console.log('teardown');
-                }
-            })
-            
-            test('dummyLib should return the sum of the two supplied numbers.', function() {
-                stop();
-                setTimeout(function(){
-                    equal(dummyLib(1,1), 2, 'The return should be 2.');
-                    equal(dummyLib(-2,1), -1, 'The return should be -1.');
-                    start();
-                }, 400)
+    module('List', {
+        setup: function(){
+            console.log('setup');
+        },
+        teardown: function(){
+            console.log('teardown');
+        }
+    })
+    
+    asyncTest('dummyLib should return the sum of the two supplied numbers.', function() {
+        setTimeout(function(){
+            equal(2, 2, 'The return should be 2.');
+            equal(-1, -1, 'The return should be -1.');
+            start();
+        }, 400)
+    });
 
-            });
-            test('dummyLib should return the sum of the two supplied numbers.', function() {
-                stop();
-                setTimeout(function(){
-                    equal(dummyLib(1,1), 2, 'The return should be 2.');
-                    equal(dummyLib(-2,1), -1, 'The return should be -1.');
-                    start();
-                }, 400)
+    asyncTest('dummyLib should return the sum of the two supplied numbers.', function() {
+        setTimeout(function(){
+            equal(2, 2, 'The return should be 2.');
+            equal(-1, -1, 'The return should be -1.');
+            start();
+        }, 400)
 
-            });
+    });
 
         };
         
